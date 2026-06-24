656a2331fa414d5c009f051a37e386ac05383e43
iluwen <iluwen@iluwendeMac-mini.local>
2026-06-24 18:47:02 +0800

    Update stashflow to 0.2.1

--- Formula/stashflow.rb
+++ Formula/stashflow.rb
@@ -1,8 +1,8 @@
 class Stashflow < Formula
   desc "TUI for cleaning Stash subscription YAML and restoring split routing"
   homepage "https://github.com/iwen-conf/stashflow"
-  url "https://github.com/iwen-conf/stashflow/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "13ca8066a59f92a8c77a62274d349f41235c85a676d550615a67a11780244dc0"
+  url "https://github.com/iwen-conf/stashflow/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "b8113695eed1df9df2bcdd14e6a101b9bf0a48de5cddbc5bf182da1a88ad4b43"
   license "MIT"
 
   depends_on "go" => :build
