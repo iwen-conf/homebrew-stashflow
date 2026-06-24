aa933ea4bd15c95d01ff01c74fa49986daa9ba0e
iluwen <iluwen@iluwendeMac-mini.local>
2026-06-24 19:31:03 +0800

    Update stashflow to 0.2.6

--- Formula/stashflow.rb
+++ Formula/stashflow.rb
@@ -1,8 +1,8 @@
 class Stashflow < Formula
   desc "TUI for cleaning Stash and Quantumult X subscription configs"
   homepage "https://github.com/iwen-conf/stashflow"
-  url "https://github.com/iwen-conf/stashflow/archive/refs/tags/v0.2.5.tar.gz"
-  sha256 "2d661268765c6ad0373603d0cf1d35f7ac3f83601b3ab0f036a6fc002037a29e"
+  url "https://github.com/iwen-conf/stashflow/archive/refs/tags/v0.2.6.tar.gz"
+  sha256 "5b4fd92a98c51fd48735a48d4bfe4852876b74bf4785c8ec4c899ebc86967fba"
   license "MIT"
 
   depends_on "go" => :build
