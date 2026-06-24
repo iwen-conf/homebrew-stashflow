4e786a40a77703c5291c899cb06aa299cefe79bf
iluwen <iluwen@iluwendeMac-mini.local>
2026-06-24 18:48:10 +0800

    Fix stashflow formula test

--- Formula/stashflow.rb
+++ Formula/stashflow.rb
@@ -1,5 +1,5 @@
 class Stashflow < Formula
-  desc "TUI for cleaning Stash subscription YAML and restoring split routing"
+  desc "TUI for cleaning Stash and Quantumult X subscription configs"
   homepage "https://github.com/iwen-conf/stashflow"
   url "https://github.com/iwen-conf/stashflow/archive/refs/tags/v0.2.1.tar.gz"
   sha256 "b8113695eed1df9df2bcdd14e6a101b9bf0a48de5cddbc5bf182da1a88ad4b43"
@@ -13,6 +13,6 @@ class Stashflow < Formula
   end
 
   test do
-    assert_match "用法:", shell_output("#{bin}/stashflow-clean --help")
+    assert_match "用法:", shell_output("#{bin}/stashflow-clean --help 2>&1")
   end
 end
