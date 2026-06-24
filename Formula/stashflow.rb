c02470b5ec4aa26cb4fd7cb6796888ff7770a75c
iluwen <iluwen@iluwendeMac-mini.local>
2026-06-24 19:00:40 +0800

    Update stashflow to 0.2.3

--- Formula/stashflow.rb
+++ Formula/stashflow.rb
@@ -1,18 +1,17 @@
 class Stashflow < Formula
   desc "TUI for cleaning Stash and Quantumult X subscription configs"
   homepage "https://github.com/iwen-conf/stashflow"
-  url "https://github.com/iwen-conf/stashflow/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "c689aec9b0b3dc4738eacb5e79933995c931210369f6543b48e1d284f8ffc74e"
+  url "https://github.com/iwen-conf/stashflow/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "33d705f54d863ea56c84073cdbee4caa4d235dbf6ee7da6b3af1797fdf8a2eb6"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(output: bin/"stashflow"), "./cmd/stashflow"
-    system "go", "build", *std_go_args(output: bin/"stashflow-clean"), "./cmd/stashflow-clean"
   end
 
   test do
-    assert_match "用法:", shell_output("#{bin}/stashflow-clean --help 2>&1")
+    assert_match "用法:", shell_output("#{bin}/stashflow --help")
   end
 end
